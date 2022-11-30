0280d3946ca681c0e799c5692befd8cc89c2f225
jureisgui <111708095+jureisgui@users.noreply.github.com>
2022-12-01 12:47:42 +1300

    start of filter system and search bar customization finished

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,8 @@
-// flarpickr code for date picker calendar
+// flatpickr code for date picker calendar
 
 config = {
     altInput: true,
-    altFormat: "F j, Y",
+    altFormat: "j F, Y",
     minDate: "today",
     // maxDate: new Date().fp_incr(15) // 15 days from now
 }
@@ -13,4 +13,27 @@ flatpickr("input[type=date]", config);
 //     "plugins": [new rangePlugin({ input: "#secondRangeInput"})]
 // })
 
+// Date difference calculation
+
+document.getElementById('search-btn').onclick = function(){
+    let date_one_val = document.getElementById('check-in-date').value;
+    let date_two_val = document.getElementById('check-out-date').value;
+    let difference = calculate_day_difference(date_one_val,date_two_val);
+    console.log(difference);
+} 
+
 // Filter accommodation options
+
+let accommodation_options = document.getElementsByClassName('accommodation-options');
+let hotel_card = document.getElementById('hotel');
+let hostel_card = document.getElementById('hostel');
+let motel_card = document.getElementById('motel');
+let house_card = document.getElementById('house');
+let quantity = document.getElementById('quantity');
+
+document.getElementById('search-btn').onclick = function(){
+    if (difference >= 1 && difference <= 5 && quantity <= 2)
+        accommodation_options.classList.remove('no-display');
+        hotel_card.classList.remove('no-display');
+    }
+    
